34f1cf99dd70d3e68a1dfefd2023723b9c0a9413
Jordan-Rose <jordan.rose@picosens.de>
2024-11-18 08:55:17 +0100

    First release of the GUI. Enable to see database, authenticate and enroll.

---
+++ gui/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace IntelRealSenseIdGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comPortLabel = new Label();
+            comPortComboBox = new ComboBox();
+            refreshButton = new Button();
+            connectButton = new Button();
+            disconnectButton = new Button();
+            statusStrip = new StatusStrip();
+            fwVersionToolStripStatusLabel = new ToolStripStatusLabel();
+            logView = new LogView();
+            authenticateButton = new Button();
+            enrollButton = new Button();
+            usersListView = new ListView();
+            idColumnHeader = new ColumnHeader();
+            usersGroupBox = new GroupBox();
+            deleteAllButton = new Button();
+            statusStrip.SuspendLayout();
+            usersGroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comPortLabel
+            // 
+            comPortLabel.AutoSize = true;
+            comPortLabel.Location = new Point(12, 15);
+            comPortLabel.Name = "comPortLabel";
+            comPortLabel.Size = new Size(77, 20);
+            comPortLabel.TabIndex = 0;
+            comPortLabel.Text = "COM port:";
+            // 
+            // comPortComboBox
+            // 
+            comPortComboBox.FormattingEnabled = true;
+            comPortComboBox.Location = new Point(95, 12);
+            comPortComboBox.Name = "comPortComboBox";
+            comPortComboBox.Size = new Size(151, 28);
+            comPortComboBox.TabIndex = 1;
+            // 
+            // refreshButton
+            // 
+            refreshButton.Location = new Point(252, 11);
+            refreshButton.Name = "refreshButton";
+            refreshButton.Size = new Size(94, 29);
+            refreshButton.TabIndex = 2;
+            refreshButton.Text = "Refresh";
+            refreshButton.UseVisualStyleBackColor = true;
+            refreshButton.Click += refreshButton_Click;
+            // 
+            // connectButton
+            // 
+            connectButton.Location = new Point(352, 11);
+            connectButton.Name = "connectButton";
+            connectButton.Size = new Size(94, 29);
+            connectButton.TabIndex = 3;
+            connectButton.Text = "Connect";
+            connectButton.UseVisualStyleBackColor = true;
+            connectButton.Click += connectButton_Click;
+            // 
+            // disconnectButton
+            // 
+            disconnectButton.Enabled = false;
+            disconnectButton.Location = new Point(452, 11);
+            disconnectButton.Name = "disconnectButton";
+            disconnectButton.Size = new Size(94, 29);
+            disconnectButton.TabIndex = 4;
+            disconnectButton.Text = "Disconnect";
+            disconnectButton.UseVisualStyleBackColor = true;
+            disconnectButton.Click += disconnectButton_Click;
+            // 
+            // statusStrip
+            // 
+            statusStrip.ImageScalingSize = new Size(20, 20);
+            statusStrip.Items.AddRange(new ToolStripItem[] { fwVersionToolStripStatusLabel });
+            statusStrip.Location = new Point(0, 393);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(900, 26);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // fwVersionToolStripStatusLabel
+            // 
+            fwVersionToolStripStatusLabel.Name = "fwVersionToolStripStatusLabel";
+            fwVersionToolStripStatusLabel.Size = new Size(95, 20);
+            fwVersionToolStripStatusLabel.Text = "FW Version: -";
+            // 
+            // logView
+            // 
+            logView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            logView.Location = new Point(491, 56);
+            logView.Name = "logView";
+            logView.Size = new Size(397, 334);
+            logView.TabIndex = 6;
+            // 
+            // authenticateButton
+            // 
+            authenticateButton.Enabled = false;
+            authenticateButton.Location = new Point(352, 56);
+            authenticateButton.Name = "authenticateButton";
+            authenticateButton.Size = new Size(128, 29);
+            authenticateButton.TabIndex = 7;
+            authenticateButton.Text = "Authenticate";
+            authenticateButton.UseVisualStyleBackColor = true;
+            authenticateButton.Click += authenticateButton_Click;
+            // 
+            // enrollButton
+            // 
+            enrollButton.Enabled = false;
+            enrollButton.Location = new Point(349, 204);
+            enrollButton.Name = "enrollButton";
+            enrollButton.Size = new Size(128, 29);
+            enrollButton.TabIndex = 8;
+            enrollButton.Text = "Enroll";
+            enrollButton.UseVisualStyleBackColor = true;
+            enrollButton.Click += enrollButton_Click;
+            // 
+            // usersListView
+            // 
+            usersListView.Columns.AddRange(new ColumnHeader[] { idColumnHeader });
+            usersListView.Dock = DockStyle.Fill;
+            usersListView.FullRowSelect = true;
+            usersListView.GridLines = true;
+            usersListView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            usersListView.Location = new Point(3, 23);
+            usersListView.MultiSelect = false;
+            usersListView.Name = "usersListView";
+            usersListView.Size = new Size(328, 318);
+            usersListView.TabIndex = 9;
+            usersListView.UseCompatibleStateImageBehavior = false;
+            usersListView.View = View.Details;
+            // 
+            // idColumnHeader
+            // 
+            idColumnHeader.Text = "User Id";
+            idColumnHeader.Width = 200;
+            // 
+            // usersGroupBox
+            // 
+            usersGroupBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            usersGroupBox.Controls.Add(usersListView);
+            usersGroupBox.Location = new Point(12, 46);
+            usersGroupBox.Name = "usersGroupBox";
+            usersGroupBox.Size = new Size(334, 344);
+            usersGroupBox.TabIndex = 10;
+            usersGroupBox.TabStop = false;
+            usersGroupBox.Text = "Users";
+            // 
+            // deleteAllButton
+            // 
+            deleteAllButton.Enabled = false;
+            deleteAllButton.Location = new Point(349, 239);
+            deleteAllButton.Name = "deleteAllButton";
+            deleteAllButton.Size = new Size(128, 29);
+            deleteAllButton.TabIndex = 11;
+            deleteAllButton.Text = "Delete all";
+            deleteAllButton.UseVisualStyleBackColor = true;
+            deleteAllButton.Click += deleteAllButton_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(900, 419);
+            Controls.Add(deleteAllButton);
+            Controls.Add(usersGroupBox);
+            Controls.Add(enrollButton);
+            Controls.Add(authenticateButton);
+            Controls.Add(logView);
+            Controls.Add(statusStrip);
+            Controls.Add(disconnectButton);
+            Controls.Add(connectButton);
+            Controls.Add(refreshButton);
+            Controls.Add(comPortComboBox);
+            Controls.Add(comPortLabel);
+            Name = "MainForm";
+            Text = "Rutronik - Intel RealSense Id Demo v1.0";
+            Load += MainForm_Load;
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            usersGroupBox.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label comPortLabel;
+        private ComboBox comPortComboBox;
+        private Button refreshButton;
+        private Button connectButton;
+        private Button disconnectButton;
+        private StatusStrip statusStrip;
+        private ToolStripStatusLabel fwVersionToolStripStatusLabel;
+        private LogView logView;
+        private Button authenticateButton;
+        private Button enrollButton;
+        private ListView usersListView;
+        private GroupBox usersGroupBox;
+        private ColumnHeader idColumnHeader;
+        private Button deleteAllButton;
+    }
+}
---
+++ gui/RealSenseCamera.cs
@@ -0,0 +1,312 @@
+﻿using rsid;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelRealSenseIdGUI
+{
+    public class RealSenseCamera
+    {
+        public enum ConnectionState
+        {
+            Iddle,
+            Error,
+            Connected
+        }
+
+        public enum AuthenticateResult
+        {
+            Spoof,
+            AccessOk,
+            AccessForbidden,
+            Error
+        }
+
+        #region "Events"
+
+        public delegate void OnNewConnectionStateEventHandler(object sender, ConnectionState state);
+        public event OnNewConnectionStateEventHandler? OnNewConnectionState;
+
+        public delegate void OnNewCameraInformationEventHandler(object sender, string firmwareVersion, string serialNumber);
+        public event OnNewCameraInformationEventHandler? OnNewCameraInformation;
+
+        public delegate void OnNewUserDatabaseEventHandler(object sender, string[] users);
+        public event OnNewUserDatabaseEventHandler? OnNewUserDatabase;
+
+        public delegate void OnNewAuthenticationEventHandler(object sender, AuthenticateResult result, string userId);
+        public event OnNewAuthenticationEventHandler? OnNewAuthentication;
+
+        #endregion
+
+        #region "Members"
+
+        private Authenticator? authenticator;
+        private string? lastError;
+
+        #endregion
+
+        /// <summary>
+        /// Initialize the connection
+        /// Open the COM port and try to read the firmware version
+        /// </summary>
+        /// <param name="portName">COM port to be opened</param>
+        /// <returns>0 on success</returns>
+        public int InitializeConnection(string portName)
+        {
+            if (authenticator != null) return -1;
+
+            DeviceController deviceController = new DeviceController();
+            Status retval = deviceController.Connect(new SerialConfig {  port = portName });
+            if (retval != Status.Ok)
+            {
+                AddError("InitializeConnection - deviceController.Connect: " + retval.ToString());
+                return -2;
+            }
+
+            string firmwareVersion = deviceController.QueryFirmwareVersion();
+            string serialNumber = deviceController.QuerySerialNumber();
+
+            if (firmwareVersion == null || serialNumber == null)
+            {
+                AddError("InitializeConnection - Cannot read firmware version or serial number.");
+                OnNewConnectionState?.Invoke(this, ConnectionState.Error);
+                deviceController.Disconnect();
+                deviceController.Dispose();
+                return -3;
+            }
+
+            OnNewCameraInformation?.Invoke(this, firmwareVersion, serialNumber);
+
+            deviceController.Disconnect();
+            deviceController.Dispose();
+
+            // SDK compatible with host version?
+            if (Authenticator.IsFwCompatibleWithHost(firmwareVersion) == false)
+            {
+                AddError(string.Format(
+                    "InitializeConnection - SDK not compatible. Should be {0} but is {1}",
+                    Authenticator.CompatibleFirmwareVersion(),
+                    firmwareVersion));
+                OnNewConnectionState?.Invoke(this, ConnectionState.Error);
+                return -4;
+            }
+
+            authenticator = new Authenticator();
+            retval = authenticator.Connect(new SerialConfig { port = portName });
+            if (retval != Status.Ok)
+            {
+                AddError("InitializeConnection - authenticator.Connect: " + retval.ToString());
+                OnNewConnectionState?.Invoke(this, ConnectionState.Error);
+                return -5;
+            }
+
+            // Set device configuration -> we want to authenticate users
+            DeviceConfig deviceConfig = new DeviceConfig();
+            deviceConfig.algoFlow = DeviceConfig.AlgoFlow.All;
+            deviceConfig.faceSelectionPolicy = DeviceConfig.FaceSelectionPolicy.Single;
+
+            retval = authenticator.SetDeviceConfig(deviceConfig);
+            if (retval != Status.Ok)
+            {
+                AddError("InitializeConnection - authenticator.SetDeviceConfig: " + retval.ToString());
+                OnNewConnectionState?.Invoke(this, ConnectionState.Error);
+                return -6;
+            }
+
+            // get the user database
+            if (GetUserIds(out string[] userIds) != 0)
+            {
+                authenticator.Disconnect();
+                authenticator.Dispose();
+                OnNewConnectionState?.Invoke(this, ConnectionState.Error);
+                return -7;
+            }
+            OnNewUserDatabase?.Invoke(this, userIds);
+
+            OnNewConnectionState?.Invoke(this, ConnectionState.Connected);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Get the list of users inside the database
+        /// </summary>
+        /// <param name="userIds"></param>
+        /// <returns></returns>
+        public int GetUserIds(out string[] userIds)
+        {
+            if (authenticator == null)
+            {
+                AddError("GetUserIds - authenticator is null");
+                userIds = [];
+                return -1;
+            }
+
+            // Get the available user ids
+            Status retval = authenticator.QueryUserIds(out userIds);
+            if (retval != Status.Ok)
+            {
+                AddError("GetUserIds: " + retval.ToString());
+                return -2;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Delete all the users
+        /// </summary>
+        /// <returns></returns>
+        public int DeleteAllUsers()
+        {
+            if (authenticator == null)
+            {
+                AddError("DeleteAllUsers - authenticator is null");
+                return -1;
+            }
+
+            Status retval = authenticator.RemoveAllUsers();
+            if (retval != Status.Ok)
+            {
+                AddError("DeleteAllUsers: retval is " + retval.ToString());
+                return -2;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Disconnect from device
+        /// </summary>
+        public void Disconnect()
+        {
+            if (authenticator == null) return;
+
+            authenticator.Disconnect();
+            authenticator.Dispose();
+            authenticator = null;
+
+            OnNewConnectionState?.Invoke(this, ConnectionState.Iddle);
+        }
+
+        /// <summary>
+        /// Add an error
+        /// </summary>
+        /// <param name="message"></param>
+        private void AddError(string message)
+        {
+            lastError = message;
+        }
+
+        /// <summary>
+        /// Get the last error
+        /// </summary>
+        /// <returns></returns>
+        public string GetLastError()
+        {
+            if (lastError == null) { return string.Empty; }
+            return lastError;
+        }
+
+        #region "Authentication callbacks"
+
+
+        void OnAuthResult(AuthStatus status, string userId, IntPtr ctx)
+        {
+            AuthenticateResult result = AuthenticateResult.Error;
+            switch(status)
+            {
+                case AuthStatus.Success:
+                    result = AuthenticateResult.AccessOk;
+
+                    break;
+                case AuthStatus.Spoof:
+                    result = AuthenticateResult.Spoof;
+                    break;
+                case AuthStatus.Forbidden:
+                    result = AuthenticateResult.AccessForbidden;
+                    break;
+            }
+            OnNewAuthentication?.Invoke(this, result, userId);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Perform an authentication
+        /// </summary>
+        /// <returns></returns>
+        public int Authenticate()
+        {
+            if (authenticator == null)
+            {
+                AddError("Authenticate - authenticator is null");
+                return -1;
+            }
+
+            var authArgs = new AuthArgs { resultClbk = OnAuthResult};
+            Status retval = authenticator.Authenticate(authArgs);
+
+            if (retval != Status.Ok)
+            {
+                AddError("Authenticate: " + retval.ToString());
+                return -2;
+            }
+
+            return 0;
+        }
+
+        #region "Enroll callbacks"
+
+        private void OnEnrollResult(EnrollStatus status, IntPtr ctx)
+        {
+            System.Diagnostics.Debug.WriteLine("OnEnrollResult " + status);
+        }
+
+        private void OnEnrollHint(EnrollStatus status, IntPtr ctx)
+        {
+            System.Diagnostics.Debug.WriteLine("OnEnrollHint " + status);
+        }
+
+        private void OnEnrollProgress(FacePose status, IntPtr ctx)
+        {
+            System.Diagnostics.Debug.WriteLine("OnEnrollProgress " + status);
+        }
+
+        private void OnEnrollFaceDetected(IntPtr faces, int count, uint ts, IntPtr ctx)
+        {
+            System.Diagnostics.Debug.WriteLine("OnEnrollFaceDetected " + count.ToString());
+        }
+
+        #endregion
+
+        public int Enroll(string userId)
+        {
+            if (authenticator == null)
+            {
+                AddError("Enroll - authenticator is null");
+                return -1;
+            }
+
+            EnrollArgs enrollArgs = new EnrollArgs { userId = userId , 
+                resultClbk = OnEnrollResult, 
+                hintClbk = OnEnrollHint,
+                progressClbk = OnEnrollProgress,
+                faceDetectedClbk = OnEnrollFaceDetected
+                };
+
+            Status retval = authenticator.Enroll(enrollArgs);
+            if (retval != Status.Ok)
+            {
+                AddError("Enroll: " + retval.ToString());
+                return -2;
+            }
+
+            return 0;
+        }
+
+    }
+}
